ff75ba0db2ff040b0a10db6ca2014c94316452a7
LAPTOP\timas <tim.a.smith486@gmail.com>
2024-08-05 10:53:07 -0230

    Fix red text in Vs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from './Navbar'
 import Home from './Home'
 import Create from './Create'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import BlogDetails from './BlogDetails'
 import NotFound from './NotFound'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   const title: string = 'Welcome to the new blogging platform!'
@@ -17,10 +17,10 @@ function App() {
         <Navbar></Navbar>
         <div className="content">
           <Routes>
-            <Route path="/" element=<Home></Home>></Route>
-            <Route path="/create" element=<Create></Create>></Route>
-            <Route path="/blogs/:id" element=<BlogDetails></BlogDetails>></Route>
-            <Route path="*" element=<NotFound></NotFound>></Route>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/create" element={<Create></Create>}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails></BlogDetails>}></Route>
+            <Route path="*" element={<NotFound></NotFound>}></Route>
           </Routes>
           {/* <h1>{title}</h1>
           <p>Liked {likes} times</p> */}
